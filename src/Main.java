0723d3294434eef29aad2cbbaccdb8c85fec18d6
MicahAndrew521 <GaydosMA22@gcc.edu>
2025-02-21 10:43:30 -0500

    adding gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,75 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+import java.util.Arrays;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    // Generate a 2^n-dimensional Hadamard Matrix (CDMA orthogonal codes)
+    public static int[][] generateHadamardMatrix(int n) {
+        int size = (int) Math.pow(2, n);
+        int[][] hadamard = new int[size][size];
+
+        // Start with H1
+        hadamard[0][0] = 1;
+
+        // Iteratively construct higher order matrices
+        for (int k = 1; k < size; k *= 2) {
+            for (int i = 0; i < k; i++) {
+                for (int j = 0; j < k; j++) {
+                    // Top-left quadrant (copy of Hk)
+                    hadamard[i][j] = hadamard[i][j];
+                    // Top-right quadrant (copy of Hk)
+                    hadamard[i][j + k] = hadamard[i][j];
+                    // Bottom-left quadrant (copy of Hk)
+                    hadamard[i + k][j] = hadamard[i][j];
+                    // Bottom-right quadrant (negated Hk)
+                    hadamard[i + k][j + k] = -hadamard[i][j];
+                }
+            }
         }
 
-        System.out.println("bounce");
-        System.out.println("Testing - Owen Commit");
+        return hadamard;
+    }
+
+    // Calculate the data for a given station index using the CDMA code
+    public static int calculateData(int[] cdmaCode, int[][] hadamardMatrix, int stationIndex) {
+        int[] stationCode = hadamardMatrix[stationIndex];
+        int data = 0;
+
+        // Perform the dot product between the CDMA code and the station code
+        for (int i = 0; i < cdmaCode.length; i++) {
+            data += cdmaCode[i] * stationCode[i];
+        }
+
+        // Normalize the result by dividing by the length of the code
+        return data / cdmaCode.length;
+    }
+
+    // Helper function to print a matrix (for debugging)
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    public static void main(String[] args) {
+        //CDMA cdma = new CDMA();
+
+        // Example: Number of devices (2^n means n=6 for 64 devices)
+        int n = 6;
+        int[][] hadamardMatrix = generateHadamardMatrix(n);
+
+        // Provided CDMA code
+        int[] cdmaCode = {
+                3540, 12, 166, 222, -262, -18, -116, -336, -172, 152, 402, -58, -122,
+                -26, 60, -228, -610, -282, -148, 132, -260, 208, 10, -194, 370, -162,
+                100, -160, -420, 132, 22, -386, 16, 44, -178, -206, -286, -142, 120,
+                16, 284, 204, -146, 70, 266, -226, -36, 248, 70, -382, 584, -36, -216,
+                120, 226, 170, -386, -26, 268, 316, -52, -216, -86, 78
+        };
+
+        // Calculate the data at the 24th station (index 23 due to 0-based indexing)
+        int stationIndex = 23; // 24th device
+        int data = calculateData(cdmaCode, hadamardMatrix, stationIndex);
+
+        // Output the result
+        System.out.println("Data at 24th station: " + data);
     }
 }
